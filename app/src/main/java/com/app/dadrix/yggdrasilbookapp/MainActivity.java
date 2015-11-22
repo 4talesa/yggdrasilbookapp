fe65198aee2b99a2eb5be522ade132450fce097f
4talesa <rondinellifortalesa@gmail.com>
2015-11-22 15:00:59 -0800

    Add basic style, applied to activities, changing from RelativeLayout to LinearLayout.

--- app/src/main/java/com/app/dadrix/yggdrasilbookapp/MainActivity.java
+++ app/src/main/java/com/app/dadrix/yggdrasilbookapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.app.dadrix.yggdrasilbookapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent it = new Intent(MainActivity.this, BookBrowseActivity.class);
+
+                MainActivity.this.startActivity(it);
+            }
+
+        }, 1000L);
     }
 }
