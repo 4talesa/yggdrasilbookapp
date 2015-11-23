ed06c5aeb152e1ae8c93b480ac1ec80bd77546b0
4talesa <rondinellifortalesa@gmail.com>
2015-11-23 11:17:33 -0800

    Add sample list on activities.

--- app/src/main/java/com/app/dadrix/yggdrasilbookapp/PlaceBrowseActivity.java
+++ app/src/main/java/com/app/dadrix/yggdrasilbookapp/PlaceBrowseActivity.java
@@ -2,6 +2,10 @@ package com.app.dadrix.yggdrasilbookapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PlaceBrowseActivity extends YggdrasilBookActivity {
 
@@ -9,5 +13,14 @@ public class PlaceBrowseActivity extends YggdrasilBookActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_browse);
+
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 0; i<10; i++){
+            items.add("Category Product #"+i);
+        }
+
+        ArrayAdapter<String> aItems = new ArrayAdapter<String>(this, R.layout.item_sample, items);
+        ListView lvTest = (ListView) findViewById(R.id.listViewPlaceBrowse);
+        lvTest.setAdapter(aItems);
     }
 }
--- app/src/main/java/com/app/dadrix/yggdrasilbookapp/PlaceDetailActivity.java
+++ app/src/main/java/com/app/dadrix/yggdrasilbookapp/PlaceDetailActivity.java
@@ -2,6 +2,12 @@ package com.app.dadrix.yggdrasilbookapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.lucasr.twowayview.TwoWayView;
+
+import java.util.ArrayList;
 
 public class PlaceDetailActivity extends YggdrasilBookActivity {
 
@@ -9,5 +15,14 @@ public class PlaceDetailActivity extends YggdrasilBookActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detail);
+
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 0; i<10; i++){
+            items.add("Category Product #"+i);
+        }
+
+        ArrayAdapter<String> aItems = new ArrayAdapter<String>(this, R.layout.item_sample, items);
+        TwoWayView lvTest = (TwoWayView) findViewById(R.id.listViewPlaceDetail);
+        lvTest.setAdapter(aItems);
     }
 }
